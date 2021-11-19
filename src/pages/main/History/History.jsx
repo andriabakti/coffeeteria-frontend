8e7f428c374771779e4d84b288f0ad8b24c97696
Andria Bakti Mahendra <andr.bkti@gmail.com>
2021-11-19 09:23:49 +0700

    Add history page's slicing

--- src/pages/main/History/History.jsx
+++ src/pages/main/History/History.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import HistoryCard from './components/HistoryCard';
+import style from './History.module.css'
 
 class History extends Component {
   render() {
     return (
-      <div className='text-center'>
-        <h1>History Page</h1>
+      <div className={style.bg}>
+        <div className={`container`}>
+          <div className={`row`}>
+            <div className={`col-md-12 py-5`}>
+              <div className={`text-center text-white ${style.head}`}>
+                <h2>Let's see what you have bought!</h2>
+                <p>Select item to delete</p>
+              </div>
+              <div className={`${style.cards}`}>
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+                <HistoryCard />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
